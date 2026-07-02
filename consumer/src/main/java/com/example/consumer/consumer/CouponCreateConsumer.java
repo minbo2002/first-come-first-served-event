6dfcc0de913c68262b249748b27284c9386c04dd
MINBO\minbo <minbo2002@naver.com>
2026-07-02 14:05:15 +0900

    feat: Consumer에서 쿠폰발급 에러 발생시 백업데이터와 로그 남기는 기능 구현

--- consumer/src/main/java/com/example/consumer/consumer/CouponCreateConsumer.java
+++ consumer/src/main/java/com/example/consumer/consumer/CouponCreateConsumer.java
@@ -1,19 +1,29 @@
 package com.example.consumer.consumer;
 
 import com.example.consumer.domain.Coupon;
+import com.example.consumer.domain.FailedEvent;
 import com.example.consumer.repository.CouponRepository;
+import com.example.consumer.repository.FailedEventRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CouponCreateConsumer {  // Topic에 전송된 데이터를 가져오기위한 Consumer 작업
 
     private final CouponRepository couponRepository;
+    private final FailedEventRepository failedEventRepository;
 
     @KafkaListener(topics = "coupon_create", groupId = "group_1")  // "coupon_create" topic에 전송된 데이터를 가져오기 위한 Listener 정의
     public void listener(Long userId) {
-        couponRepository.save(new Coupon(userId));
+        try {
+            couponRepository.save(new Coupon(userId));
+        } catch (Exception e) {
+            log.error("failed to create coupon::" + userId);
+            failedEventRepository.save(new FailedEvent(userId));
+        }
     }
 }
